2caa30122a4ab20b9f46f8bc6815cbb98d8bc9bc
PetyaGGeorgieva <pettya.gg@gmail.com>
2024-09-10 13:28:46 +0200

    first commit

---
+++ NumbersGame/NumbersGame/NumbersGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersGameApp.swift
+//  NumbersGame
+//
+//  Created by Petya Georgieva on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NumbersGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ components/PrimaryButton.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function PrimaryButton({ children }) {
+  return <View>
+    <Text>{children}</Text>
+  </View>
+}
+
+export default PrimaryButton;
